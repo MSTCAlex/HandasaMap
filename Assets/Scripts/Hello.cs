bdb801b47dc5b90de51db366783027e277c6da5d
Mohammed AbuIssa <Mohammed AbuIssa>
2016-02-13 19:49:19 +0200

    Added Plane prefab, each Plane is a node, there is an example of traversing the graph in hello.cs script, you should attach it to game object and use a button to call the hi(), #ToDo shortest path

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hello : MonoBehaviour
+{
+	public GameObject Begin;
+	public GameObject Target;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	public void Hi()
+	{
+		TestList l = Begin.GetComponent<TestList> ();
+		TestList.Result R = l.Find (Target, Begin);
+		if (R.isFound)
+			foreach (var item in R.Parents) {
+				Debug.Log (item.name);
+			}
+	}
+}
---
+++ Assets/Scripts/TestList.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class TestList : MonoBehaviour {
+	private GameObject Parent;
+	private bool Visited = false;
+	public List<GameObject> Neighbors = new List<GameObject> ();
+	// Use this for initialization
+	void Start () {
+		Parent = this.gameObject;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public class Result{
+		public List<GameObject> Parents {
+			get;
+			set;
+		}
+
+		public bool isFound {
+			get;
+			set;
+		}
+		public Result ()
+		{
+			Parents = new List<GameObject>();
+		}
+		public Result (bool isFound) : this()
+		{
+			this.isFound = isFound;
+		} 
+	}
+	public Result Find (GameObject go, GameObject Caller){
+		Result R;
+		if (!this.Visited) {
+			Visited = true;
+			if (Parent == go) {
+				R = new Result (true);
+				R.Parents.Add (Parent);
+				return R;
+			} else {
+				foreach (var item in Neighbors) {
+					if (item != Caller) {
+						R =  item.GetComponent<TestList>().Find(go, Parent);
+						if (R.isFound) {
+							R.Parents.Add (Parent);
+							return R;
+						}
+					}
+					continue;
+				}
+
+			}
+		}
+		R = new Result (false);
+		return R;
+	}
+
+}
