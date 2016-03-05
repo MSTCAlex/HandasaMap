da51b294cab534114bf27d0f7fb8b13f2bcf080b
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-03-05 20:03:02 +0200

    Add button property to Place Panel

---
+++ Assets/Scripts/TestUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI : MonoBehaviour {
+    
+    public string placeName;
+
+    public void PlaceName()
+    {
+        Debug.Log(placeName);
+    }
+    
+}
